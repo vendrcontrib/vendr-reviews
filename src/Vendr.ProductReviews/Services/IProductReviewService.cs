fb1c5a493c72ffb307541b36f85bc8bb3fe68c07
Bjarne Fyrstenborg <bf@oerskov-gruppen.lan>
2020-09-17 21:07:50 +0200

    Add foundation for product reviews

---
+++ src/Vendr.ProductReviews/Services/IProductReviewService.cs
@@ -0,0 +1,25 @@
+﻿namespace Vendr.ProductReviews.Services
+{
+    interface IProductReviewService
+    {
+        /// <summary>
+        /// Gets a product review.
+        /// </summary>
+        void GetProductReview(int id);
+
+        /// <summary>
+        /// Gets product reviews.
+        /// </summary>
+        void GetProductReviews(string productReference);
+
+        /// <summary>
+        /// Gets product reviews for customer.
+        /// </summary>
+        void GetProductReviewsForCustomer(string customerReference);
+
+        /// <summary>
+        /// Add product review.
+        /// </summary>
+        void AddProductReview(string productReference, string customerReference, decimal rating, string name, string description);
+    }
+}
---
+++ src/Vendr.ProductReviews/Web/UI/App_Plugins/VendrProductReviews/BackOffice/resources/vendr-product-reviews.resource.js
@@ -0,0 +1,26 @@
+﻿(function() {
+
+    'use strict';
+
+    function vendrProductReviewsResource($http, umbRequestHelper) {
+
+        return {
+
+            getProductReview: function (id) {
+
+            },
+
+            getProductReviews: function(productReference) {
+                
+            },
+
+            getProductReviewsForCustomer: function (customerReference) {
+
+            }
+        };
+
+    };
+
+    angular.module('vendr.resources').factory('vendrProductReviewsResource', vendrProductReviewsResource);
+
+}());
