60436e555ea64efdd1d5b0e1e5d2fde403550e5d
Bjarne Fyrstenborg <bf@oerskov.dk>
2020-09-18 10:21:06 +0200

    Rename to Vendr.Contrib.ProductReviews

--- src/Vendr.Contrib.ProductReviews/Properties/AssemblyInfo.cs
+++ src/Vendr.Contrib.ProductReviews/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vendr.ProductReviews")]
+[assembly: AssemblyTitle("Vendr.Contrib.ProductReviews")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vendr.ProductReviews")]
+[assembly: AssemblyProduct("Vendr.Contrib.ProductReviews")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
