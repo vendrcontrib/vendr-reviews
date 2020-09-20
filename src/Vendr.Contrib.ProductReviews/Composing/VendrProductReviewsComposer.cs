49c30f3441e0df0b4a1b47bd84e4619d6b551f89
Bjarne Fyrstenborg <bf@oerskov-gruppen.lan>
2020-09-20 18:45:11 +0200

    Register dependencies

--- src/Vendr.Contrib.ProductReviews/Composing/VendrProductReviewsComposer.cs
+++ src/Vendr.Contrib.ProductReviews/Composing/VendrProductReviewsComposer.cs
@@ -1,22 +1,30 @@
-﻿using Umbraco.Core;
+﻿using LightInject;
+using Umbraco.Core;
 using Umbraco.Core.Composing;
+using Vendr.Contrib.ProductReviews.Factories;
+using Vendr.Contrib.ProductReviews.Persistence.Repositories;
+using Vendr.Contrib.ProductReviews.Persistence.Repositories.Implement;
 using Vendr.ProductReviews.Services;
 using Vendr.ProductReviews.Services.Implement;
 using Vendr.Web.Composing;
 
 namespace Vendr.Contrib.ProductReviews.Composing
 {
+    //[RuntimeLevel(MinLevel = RuntimeLevel.Run)]
     [ComposeAfter(typeof(VendrWebComposer))]
     public class VendrProductReviewsComposer : IUserComposer
     {
         public void Compose(Composition composition)
         {
+            composition.RegisterUnique<IProductReviewRepositoryFactory>();
+            composition.RegisterUnique<IProductReviewRepository, ProductReviewRepository>();
+
             // Register services
             composition.Register<IProductReviewService, ProductReviewService>(Lifetime.Singleton);
 
             // Register component
-            //composition.Components()
-            //    .Append<VendrProductReviewsComponent>();
+            composition.Components()
+                .Append<VendrProductReviewsComponent>();
         }
     }
 }
