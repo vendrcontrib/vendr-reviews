6ac8f1428ff6fe07a3a5cfb6bfe90828ce0a1e30
Bjarne Fyrstenborg <bf@oerskov-gruppen.lan>
2020-09-20 15:36:39 +0200

    Compose after VendrWebComposer

--- src/Vendr.Contrib.ProductReviews/Composing/VendrProductReviewsComposer.cs
+++ src/Vendr.Contrib.ProductReviews/Composing/VendrProductReviewsComposer.cs
@@ -2,15 +2,17 @@
 using Umbraco.Core.Composing;
 using Vendr.ProductReviews.Services;
 using Vendr.ProductReviews.Services.Implement;
+using Vendr.Web.Composing;
 
 namespace Vendr.Contrib.ProductReviews.Composing
 {
+    [ComposeAfter(typeof(VendrWebComposer))]
     public class VendrProductReviewsComposer : IUserComposer
     {
         public void Compose(Composition composition)
         {
             // Register services
-            composition.Register<IProductReviewService, ProductReviewService>();
+            composition.Register<IProductReviewService, ProductReviewService>(Lifetime.Singleton);
 
             // Register component
             //composition.Components()
