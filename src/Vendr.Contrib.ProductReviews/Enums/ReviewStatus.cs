15ea313e209ef0a37c3a3907a8ea6c55f45ba8c2
Bjarne Fyrstenborg <bf@oerskov-gruppen.lan>
2020-09-21 23:07:38 +0200

    Decorate enum

--- src/Vendr.Contrib.ProductReviews/Enums/ReviewStatus.cs
+++ src/Vendr.Contrib.ProductReviews/Enums/ReviewStatus.cs
@@ -1,9 +1,20 @@
-﻿namespace Vendr.ProductReviews.Enums
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace Vendr.ProductReviews.Enums
 {
+    [DataContract]
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum ReviewStatus
     {
+        [EnumMember(Value = "pending")]
         Pending,
+
+        [EnumMember(Value = "approved")]
         Approved,
+
+        [EnumMember(Value = "declined")]
         Declined
     }
 }
