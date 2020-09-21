6ca2e181066259b55d043e208cf7c99b8f65d201
Bjarne Fyrstenborg <bf@oerskov-gruppen.lan>
2020-09-21 21:07:27 +0200

    Update injecting tree nodes

--- src/Vendr.Contrib.ProductReviews/Composing/VendrProductReviewsComponent.cs
+++ src/Vendr.Contrib.ProductReviews/Composing/VendrProductReviewsComponent.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Umbraco.Core.Composing;
 using Umbraco.Web;
 using Umbraco.Web.Trees;
@@ -33,20 +34,39 @@ namespace Vendr.Contrib.ProductReviews.Composing
         {
             // normally you will want to target a specific tree, this can be done by checking the
             // tree alias of by checking the tree type (casting 'sender')
-            if (sender.TreeAlias == "vendr" && e.Nodes.Any(n => n.NodeType == "Store"))
+            if (sender.TreeAlias == "vendr")
             {
-                var stores = e.Nodes.Where(n => n.NodeType == "Store").ToList();
+                var mainRoute = "commerce/vendr";
 
-                if (stores != null && stores.Any())
+                var stores = e.Nodes.Where(n => n.NodeType == Vendr.Core.Constants.Entities.EntityTypes.Store).ToList();
+
+                foreach (var node in stores)
                 {
-                    var mainRoute = "/commerce/vendr";
-
-                    foreach (var store in stores)
+                    var parentId = node.Id.ToString();
+                    if (parentId == "b1e61994-b83b-420a-903e-63a7a15942dc")
                     {
-                        var childNode = sender.CreateTreeNode("reviews", store.Id.ToString(), e.QueryStrings, "Reviews", "icon-rate", false, $"{mainRoute}/reviews");
+                        var id = Guid.NewGuid().ToString();
+                        var childNode = sender.CreateTreeNode(id, parentId, e.QueryStrings, "Reviews", "icon-rate", false, $"{mainRoute}/reviews/{id}");
+                        childNode.Path = $"-1,{parentId},{id}";
                         e.Nodes.Insert(e.Nodes.Count - 1, childNode);
                     }
                 }
+
+                //var stores = e.Nodes.Where(n => n.NodeType == "Store").ToList();
+
+                //if (stores != null && stores.Any())
+                //{
+                //    var mainRoute = "commerce/vendr";
+
+                //    foreach (var store in stores)
+                //    {
+                //        var parentId = store.Id.ToString();
+                //        var id = Guid.NewGuid().ToString();
+                //        var childNode = sender.CreateTreeNode(id, parentId, e.QueryStrings, "Reviews", "icon-rate", false, $"{mainRoute}/reviews");
+                //        childNode.Path = $"-1,{parentId},{id}";
+                //        e.Nodes.Insert(0, childNode);
+                //    }
+                //}
 
                 //var stores = _storeService.GetStores();
                 //if (stores != null && stores.Any())
