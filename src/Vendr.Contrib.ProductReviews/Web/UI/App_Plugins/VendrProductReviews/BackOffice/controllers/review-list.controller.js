b2cebab010e94300f9e2bf58a9bba93fbc640f54
Bjarne Fyrstenborg <bf@oerskov-gruppen.lan>
2020-09-23 09:58:00 +0200

    Adjust styling of status

--- src/Vendr.Contrib.ProductReviews/Web/UI/App_Plugins/VendrProductReviews/BackOffice/controllers/review-list.controller.js
+++ src/Vendr.Contrib.ProductReviews/Web/UI/App_Plugins/VendrProductReviews/BackOffice/controllers/review-list.controller.js
@@ -96,7 +96,7 @@
                         </span><span class="sr-only">{{rating}} stars</span></span>` },
                 { alias: 'review', header: 'Review', template: '<span class="db bold">{{title}}</span><span class="vendr-table-cell-label">{{description}}</span>' },
                 { alias: 'createDate', header: 'Date', template: "{{ createDate | date : 'MMMM d, yyyy h:mm a' }}" },
-                { alias: 'status', header: 'Status', align: 'right', template: '<span class="umb-badge umb-badge--xs vendr-bg--blue" title="Status: {{ status }}">{{ status }}</span>' }
+                { alias: 'status', header: 'Status', align: 'right', template: '<span class="umb-badge umb-badge--xs vendr-badge-{{ status.toLowerCase() }}" title="Status: {{status}}">{{status}}</span>' }
             ],
             itemClick: function (itm) {
                 console.log("routePath", itm.routePath);
