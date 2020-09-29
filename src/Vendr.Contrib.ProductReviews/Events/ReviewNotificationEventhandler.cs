ef3c76adb029619946b9f357a3ddbc4a688b803a
Bjarne Fyrstenborg <bf@oerskov-gruppen.lan>
2020-09-29 13:04:37 +0200

    Inject IProductAdapter

--- src/Vendr.Contrib.ProductReviews/Events/ReviewNotificationEventhandler.cs
+++ src/Vendr.Contrib.ProductReviews/Events/ReviewNotificationEventhandler.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Vendr.Core.Adapters;
 using Vendr.Core.Events.Notification;
 using Vendr.Core.Logging;
 
@@ -7,20 +7,25 @@ namespace Vendr.Contrib.ProductReviews.Events
     public class ProductReviewAddedHandler : NotificationEventHandlerBase<ProductReviewAddedNotification>
     {
         private readonly IActivityLogger _activityLogger;
+        private readonly IProductAdapter _productAdapter;
 
-        public ProductReviewAddedHandler(IActivityLogger activityLogger)
+        public ProductReviewAddedHandler(IActivityLogger activityLogger, IProductAdapter productAdapter)
         {
             _activityLogger = activityLogger;
+            _productAdapter = productAdapter;
         }
 
         public override void Handle(ProductReviewAddedNotification evt)
         {
+            var snapshot = _productAdapter.GetProductSnapshot(evt.Review.ProductReference, "GB");
+
             _activityLogger.LogActivity(evt.Review.StoreId,
                 evt.Review.Id, 
                 Constants.Entities.EntityTypes.Review, 
                 "New review added",
                 $"vendrproductreviews/review-edit/{evt.Review.StoreId}_{evt.Review.Id}",
-                $"Review submitted from {evt.Review.Name} with a rating {evt.Review.Rating}.");
+                $"Review submitted from {evt.Review.Name} with a rating {evt.Review.Rating} for product {snapshot.Sku}.",
+                evt.Review.CreateDate);
         }
     }
 }
